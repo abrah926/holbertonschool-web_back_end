03ca0d304f4a776d7e909f7597a580aebda9f7d8
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-11-13 11:32:38 -0400

    implementing static methods

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,51 @@
+import Currency from "./3-currency";
+
+
+class Pricing {
+    constructor(amount, currency) {
+        if (typeof amount!== 'number' || amount < 0) {
+            throw new TypeError("Amount must be a non-negative number");
+        }
+        if (!(currency instanceof Currency)) {
+            throw new TypeError("currency must be an instance of Currency");
+        }
+        this._amount = amount;
+        this._currency = currency;
+
+
+    }
+
+    static convertPrice(amount, conversionRate) {
+
+        if (typeof conversionRate!== 'number' || conversionRate <= 0) {
+            throw new TypeError("Conversion rate must be a positive number");
+        }
+        if (typeof amount!== 'number') {
+            throw new TypeError("Amount must be a number");
+        }
+        return amount * conversionRate;
+
+    }
+    get amount() {
+        return this._amount;
+    }
+    set amount(amount) {
+        if (typeof amount!== 'number' || amount < 0) {
+            throw new TypeError("Amount must be a non-negative number");
+        }
+        this._amount = amount;
+    }
+    get currency() {
+        return this._currency;
+    }
+    set currency(currency) {
+        if (!(currency instanceof Currency)) {
+            throw new TypeError("currency must be an instance of Currency");
+        }
+        this._currency = currency;
+    }
+    displayFullPrice() {
+        return (`${this._amount} ${this._currency.name} (${this._currency.code})`)
+    }
+
+}
