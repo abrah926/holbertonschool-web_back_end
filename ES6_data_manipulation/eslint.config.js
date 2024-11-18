8f912f08e7ac85c1b8205a1c50fba3835e4b0885
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-11-18 10:45:53 -0400

    eslint

--- ES6_data_manipulation/eslint.config.js
+++ ES6_data_manipulation/eslint.config.js
@@ -1,10 +1,9 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    files: ['**/*.js'],
-    rules: {
-      'no-console': 'warn',
-      'eqeqeq': 'error',
-      'strict': ['error', 'global']
-    },
-  },
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
 ];
