84f5795f8a25930f420f36be232f1389f6fdbcf3
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-11-18 12:49:59 -0400

    returns students with matching cities

---
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,12 @@
+export default getStudentsByLocation(student, city) {
+    if (!Array.isArray(student)) {
+        throw new Error("Invalid input: student must be an array");
+    }
+
+    if (typeof city !== "string") {
+        throw new Error("Invalid input: city must be a string");
+    }
+
+
+    return students.filter(student => student.location === city);
+}
