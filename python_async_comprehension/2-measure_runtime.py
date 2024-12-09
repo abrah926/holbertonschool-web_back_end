f72b532178cc2fa823c37416e14fe7fa3bf7d3f3
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-12-09 14:25:51 -0400

    updated type annotations

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -7,7 +7,7 @@ import asyncio
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """
     Measure the runtime of running async_comprehension 4 times in parallel.
     """
