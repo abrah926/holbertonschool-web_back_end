1c9f93aa7f51db3d1962010df7403da07807b6a8
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-12-09 14:45:25 -0400

    returns tuple

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -3,10 +3,17 @@
 from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     Returns a tuple containing the start and end indices for a specific page of a paginated list.
 
     Args:
-    page (int): The index of the page (starting from 1)."""
-    return tuple(page, page_size)
+        page (int): The index of the page (starting from 1).
+        page_size (int): The number of items per page.
+
+    Returns:
+        Tuple[int, int]: A tuple containing the start and end indices.
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return start_index, end_index
