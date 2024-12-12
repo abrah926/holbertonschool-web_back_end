cbb243b024a862f76e91393235129c4b70af34c4
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-12-12 16:15:47 -0400

    list_all function to list all documents

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+"""
+Module 8-all
+Defines the list_all function to list all documents in a MongoDB collection.
+"""
+
+
 def list_all(mongo_collection):
     """
     Lists all documents in a MongoDB collection.
@@ -12,5 +18,4 @@ def list_all(mongo_collection):
     """
     if mongo_collection is None:
         return []
-
     return list(mongo_collection.find())
