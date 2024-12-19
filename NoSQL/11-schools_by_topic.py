16daf7fffc24aa5c4db0ef4dcd94aa830c3f5c70
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-12-19 11:26:21 -0400

    list of schools

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -5,5 +5,11 @@ from pymongo.collection import Collection
 
 def schools_by_topic(mongo_collection: Collection, topic: str):
     """Return the list of schools having a specific topic."""
+    # Ensure uniqueness by filtering duplicates
     schools = mongo_collection.find({"topics": topic})
-    return [{"_id": str(school.get("_id")), "name": school.get("name"), "topics": school.get("topics", [])} for school in schools]
+    unique_schools = {school.get("_id"): school for school in schools}
+    return [
+        {"_id": str(school_id), "name": school.get("name"),
+         "topics": school.get("topics", [])}
+        for school_id, school in unique_schools.items()
+    ]
