5f3c5f58048905c3516583ca84f1cb64d039ec66
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-12-19 11:08:28 -0400

    Return the list of all the delays

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,22 +1,13 @@
+#!/usr/bin/env python3
+""" Tasks """
+
 import asyncio
-import random
 from typing import List
 
-
-async def wait_random(max_delay: int = 10) -> float:
-    """An asynchronous coroutine that waits for a random delay between 0 and max_delay seconds."""
-    delay = random.uniform(0, max_delay)
-    await asyncio.sleep(delay)
-    return delay
-
-
-def task_wait_random(max_delay: int) -> asyncio.Task:
-    """A function that returns an asyncio.Task for wait_random."""
-    return asyncio.create_task(wait_random(max_delay))
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Run wait_random n times concurrently using task_wait_random and return delays in ascending order."""
-    tasks = [task_wait_random(max_delay) for _ in range(n)]
-    delays = [await task for task in asyncio.as_completed(tasks)]
-    return delays
+    """ Return the list of all the delays """
+    delays = [wait_random(max_delay) for _ in range(n)]
+    return [await delay for delay in asyncio.as_completed(delays)]
