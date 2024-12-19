64ee96274d5432142af61a999962cb17c3f7ccf8
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-12-19 11:27:51 -0400

    return by topic

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,15 +1,7 @@
 #!/usr/bin/env python3
+""" 11-schools_by_topic """
 
-from pymongo.collection import Collection
 
-
-def schools_by_topic(mongo_collection: Collection, topic: str):
-    """Return the list of schools having a specific topic."""
-    # Ensure uniqueness by filtering duplicates
-    schools = mongo_collection.find({"topics": topic})
-    unique_schools = {school.get("_id"): school for school in schools}
-    return [
-        {"_id": str(school_id), "name": school.get("name"),
-         "topics": school.get("topics", [])}
-        for school_id, school in unique_schools.items()
-    ]
+def schools_by_topic(mongo_collection, topic):
+    """returns the list of school having a specific topic"""
+    return mongo_collection.find({"topics": topic})
