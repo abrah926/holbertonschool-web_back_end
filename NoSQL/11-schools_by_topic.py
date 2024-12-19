45b8a4ae15bdb3a6da84bb5ab761253cd79c5de6
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-12-19 11:15:08 -0400

    Return the list of schools having a specific topic

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,20 +1,7 @@
-from pymongo import MongoClient
+from pymongo.collection import Collection
+from typing import List
 
 
-def insert_school(mongo_collection, **kwargs):
-    """Insert a document into the collection."""
-    return mongo_collection.insert_one(kwargs).inserted_id
-
-
-def schools_by_topic(mongo_collection, topic):
-    """
-    Find all schools that have a specific topic.
-
-    Args:
-        mongo_collection: The pymongo collection object.
-        topic (str): The topic to search for.
-
-    Returns:
-        List of schools (documents) matching the topic.
-    """
-    return mongo_collection.find({"topics": topic})
+def schools_by_topic(mongo_collection: Collection, topic: str) -> List[dict]:
+    """Return the list of schools having a specific topic."""
+    return list(mongo_collection.find({"topics": topic}))
