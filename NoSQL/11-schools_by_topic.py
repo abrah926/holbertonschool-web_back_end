1a254526d8839e664e7379be06301a7aa47804e9
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-12-19 11:18:45 -0400

    Return the list of schools having a specific topic

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 from pymongo.collection import Collection
-from typing import List
 
 
-def schools_by_topic(mongo_collection: Collection, topic: str) -> List[dict]:
+def schools_by_topic(mongo_collection: Collection, topic: str):
     """Return the list of schools having a specific topic."""
-    return list(mongo_collection.find({"topics": topic}))
+    schools = mongo_collection.find({"topics": topic})
+    return [{"_id": str(school.get("_id")), "name": school.get("name"), "topics": school.get("topics", [])} for school in schools]
