b39ede46e87f7e8bc937feb085363956867d4b39
abrah926 <abrahamvidalcastillo2@gmail.com>
2025-02-23 07:44:38 -0400

    Add HTTP server implementation in 4-http.js

--- Node_JS_basic/1-stdin.js
+++
@@ -1,9 +0,0 @@
-console.log('Welcome to Holberton School, what is your name?');
-
-process.stdin.on('data', (data) => {
-  const input = data.toString().trim();
-  console.log(`Your name is: ${input}`);
-
-  console.log('This important software is now closing');
-  process.exit(0);
-});
--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -2,15 +2,15 @@ const http = require('http');
 
 // Create the server
 const app = http.createServer((req, res) => {
-    res.statusCode = 200; // Set status code to 200 (OK)
-    res.setHeader('Content-Type', 'text/plain'); // Set plain text response
-    res.end('Hello Holberton School!');
+  res.statusCode = 200; // Set status code to 200 (OK)
+  res.setHeader('Content-Type', 'text/plain'); // Set plain text response
+  res.end('Hello Holberton School!');
 });
 
 // Set the server to listen on port 1245
 const PORT = 1245;
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}/`);
+  console.log(`Server running at http://localhost:${PORT}/`);
 });
 
 // Export the app
